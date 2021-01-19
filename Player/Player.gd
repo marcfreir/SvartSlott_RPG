ddf9ba6a6e9b573cbcf5abc300b9ac3434551f97
marcfreir <marcfreir@outlook.com>
2021-01-19 20:30:03 -0200

    Initial commit

---
+++ Player/Player.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+# func _ready():
+	# print("Hello World!")
+	# pass # Replace with function body.
+
+func _physics_process(delta):
+	print("Hello World!")
