de8dca9047049f71ae37e66556d593952c8a56b6
marcfreir <marcfreir@outlook.com>
2021-01-19 23:29:54 -0200

    Add key event handling. Update & Refactor

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+var velocity = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 # func _ready():
@@ -7,4 +8,16 @@ extends KinematicBody2D
 	# pass # Replace with function body.
 
 func _physics_process(delta):
-	print("Hello World!")
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = 4
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -4
+	elif Input.is_action_pressed("ui_up"):
+		velocity.y = -4
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = 4
+	else:
+		velocity.x = 0
+		velocity.y = 0
+		
+	move_and_collide(velocity)
