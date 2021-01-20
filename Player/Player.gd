570fca7e6481ec1cedbaadaac39c83d06dd070fa
marcfreir <marcfreir@outlook.com>
2021-01-20 17:30:06 -0200

    Enhance & simplify movements logic of the player

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,6 +2,10 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 
+const MAX_SPEED = 80
+const ACCELERATION = 500
+const FRICTION = 500
+
 # Called when the node enters the scene tree for the first time.
 # func _ready():
 	# print("Hello World!")
@@ -11,11 +15,14 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	# print(input_vector)
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
+	print(velocity)
 		
-	move_and_collide(velocity)
+	move_and_collide(velocity * delta)
